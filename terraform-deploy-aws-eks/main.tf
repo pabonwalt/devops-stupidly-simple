37a599786ad767caa44f9bd0e409617f63a61185
Pepe Grillo <walter@centre>
2022-11-30 01:50:54 -0500

    improvements a lot

---
+++ terraform-deploy-aws-eks/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {}
+
+resource "digitalocean_kubernetes_cluster" "k8scluster01" {
+  name   = "k8scluster01"
+  region = "sfo3"
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.24.4-do.0"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-4gb-intel"
+    node_count = 2
+
+    taint {
+      key    = "workloadKind"
+      value  = "database"
+      effect = "NoSchedule"
+    }
+  }
+}
