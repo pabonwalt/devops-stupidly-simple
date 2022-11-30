a44e0150701abad6c6e262df6cd0ede2c1df36ac
Walter Pabon <walter@centre>
2022-11-30 12:24:30 -0500

    improvements

--- terraform-deploy-aws-eks/variables.tf
+++ terraform-deploy-aws-eks/variables.tf
@@ -1,5 +1,9 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-2"
-}
+ variable "subnet_id_1" {
+  type = string
+  default = "subnet-0f412098a6aa9f286"
+ }
+ 
+ variable "subnet_id_2" {
+  type = string
+  default = "subnet-06badb546781ea9d1"
+ }
